64ebba19597c7269705c2f8b21a62d5a1b7f732a
Aylmer Bolzan <110077540+aylmerbolzan@users.noreply.github.com>
2022-11-06 08:39:34 -0300

    Atualização de click para toque

--- js/script.js
+++ js/script.js
@@ -31,4 +31,4 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
 }, 10);
 
-document.addEventListener('click', jump);
+document.addEventListener('ontouchstart', jump);
